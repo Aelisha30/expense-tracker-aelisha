74bb62be4c08f612ba5d4f32732414701975abc8
Aelisha30 <147382061+Aelisha30@users.noreply.github.com>
2025-01-22 21:18:27 +0530

    Update useGetUserInfo.js

--- src/hooks/useGetUserInfo.js
+++ src/hooks/useGetUserInfo.js
@@ -1,8 +1,25 @@
-export const useGetUserInfo=()=>{
+/*export const useGetUserInfo=()=>{
     const{name,profilePhoto,userID,isAuth}=JSON.parse(
         localStorage.getItem("auth")
     );
     
 return {name,profilePhoto,userID,isAuth};
-};
+};*/
+export const useGetUserInfo = () => {
+    const authData = localStorage.getItem("auth");
+  
+    
+    if (!authData) {
+      return { name: "", profilePhoto: "", userID: "", isAuth: false };
+    }
+  
+    try {
+      const { name, profilePhoto, userID, isAuth } = JSON.parse(authData);
+      return { name, profilePhoto, userID, isAuth };
+    } catch (error) {
+      console.error("Error parsing auth data:", error);
+      return { name: "", profilePhoto: "", userID: "", isAuth: false };
+    }
+  };
+
 
